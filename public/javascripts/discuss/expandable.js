b8499b54a803447f17cce913c0874e1756cae66a
Tiago Cardoso <tiago.cardoso@echologic.org>
2012-01-29 01:24:55 +0100

    rewrote the expandable js plugin into a js widget

--- public/javascripts/discuss/expandable.js
+++ public/javascripts/discuss/expandable.js
@@ -1,154 +1,121 @@
-(function($) {
-
-  $.fn.expandable = function(current_settings) {
-
-     $.fn.expandable.defaults = {
-			'animate': true,
-			'animation_params' : {
-        'height' : 'toggle',
-        'opacity': 'toggle'
-      },
-      'animation_speed': 300,
-			'loading_class': '.loading',
-			'parent_class' : 'div:first',
-
+(function( $ ) {
+	
+	$.widget("echo.expandable", {
+		options: {
+			animate: true,
+			animation_params : {
+        		height : 'toggle',
+        		opacity: 'toggle'
+      		},
+      		animation_speed: 300,
+			loading_class: '.loading',
+			parent_class : 'div:first',
 			// SPECIAL CONDITION ELEMENTS
-			'condition_element': null,
-      'condition_class': ''
-    };
+			condition_element: null,
+      		condition_class: ''
+		},
+		_create: function() {
+			var that = this,
+				expandable = that.element;
+				
+			that.expandableParent = expandable.parents(that.options.parent_class);
+			that.expandableSupportersLabel = expandable.find('.supporters_label');
+			that.expandableLoading = expandable.parent().find(that.options.loading_class);
+			that.expandableContent = that.expandableParent.children('.expandable_content');
 
-    // Merging settings with defaults
-    var settings = $.extend({}, $.fn.expandable.defaults, current_settings);
+			that.path = expandable.attr('href') || expandable.data('href');
+			expandable.removeData('href');
+			
+			if(that.expandableContent.length > 0) 
+				expandable.addClass("active");
+				
+			// Collapse/expand clicks
+        	expandable.bind("click", function(e){
+        		e.preventDefault();
+        		e.stopPropagation();
+				if (!that.options.condition_element) 
+					that._toggleExpandable();
+				else if(that.options.condition_element.hasClass(that.options.condition_class))
+					that.options.condition_element.hasClass('pending') ? 
+					expandable.addClass('clicked') :
+					that._toggleExpandable();
+			});
+			
+		},
+		_toggleExpandable: function() {
+			var that = this,
+				expandable = that.element;
+				
+        	if (that.expandableContent.length > 0) {
+				// Content is already loaded
+				expandable.toggleClass('active');
+				
+				that.options.animate ?
+				that.expandableContent.animate(that.options.animation_params, that.options.animation_speed) :
+				expandable_content.toggle();
+				
+				
+				if (that.expandableSupportersLabel.length > 0) 
+					that.options.animate ?
+					that.expandableSupportersLabel.animate(that.options.animation_params, that.options.animation_speed) : 
+					that.expandableSupportersLabel.toggle();
 
-    return this.each(function() {
 
-	    // Creating expandable and binding its API
-	    var elem = $(this);
-      var expandableApi = elem.data('expandableApi');
-	    if (expandableApi) {
-				expandableApi.reinitialise();
-	    } else {
-	      expandableApi = new Expandable(elem);
-	      elem.data('expandableApi', expandableApi);
-	    }
-		});
-
-
-    /* The expandable handler */
-    function Expandable(expandable) {
-			var expandable_parent = expandable.parents(settings['parent_class']);
-			var expandable_supporters_label = expandable.find('.supporters_label');
-			var expandable_loading = expandable.parent().find(settings['loading_class']);
-			var expandable_content = expandable_parent.children('.expandable_content');
-
-			var path = expandable.attr('href') || expandable.data('href');
-			expandable.removeAttr('href').removeData('href');
-
-      initialise();
-
-      function initialise() {
-				if(expandable_content.length > 0) {
-					expandable.addClass("active");
+			} else if (!expandable.hasClass('pending')) {
+				expandable.addClass('pending');
+			
+				// Load content now
+				if (that.expandableLoading.length > 0) 
+					that.expandableLoading.show();
+				else {
+					that.expandableLoading = $('<span/>').addClass('loading');
+					that.expandableLoading.insertAfter(expandable);
 				}
-			  // Collapse/expand clicks
-        expandable.bind("click", function(){
-					if (!settings['condition_element']) {
-						toggleExpandable();
-					} else if(settings['condition_element'].hasClass(settings['condition_class'])) {
-						if(settings['condition_element'].hasClass('pending')) {
-							expandable.addClass('clicked');
-						} else {
-							toggleExpandable();
-						}
+				$.ajax({
+					url: that.path,
+					type: 'get',
+					dataType: 'script',
+					success: function(){
+						that._activate();
+					},
+					error: function(){
+						expandable_loading.hide();
+						expandable.removeClass('pending');
 					}
-          return false;
-        });
+				});
 			}
+		},
+		// Designates the expandable as active.
+      	_activate: function () {
+      		var that = this,
+      			expandable = that.element;
+        	
+        	that.expandableLoading.hide();
+        	that.expandableContent = that.expandableParent.children('.expandable_content');
+        	if (that.expandableContent.length > 0) 
+          		expandable.addClass('active');
+          		
+        	if (that.expandableSupportersLabel) 
+          		that.options.animate ?
+            	that.expandableSupportersLabel.animate(that.options.animation_params, that.options.animation_speed) :
+            	that.expandableSupportersLabel.toggle();
 
-			function toggleExpandable () {
-        if (expandable_content.length > 0) {
-					// Content is already loaded
-					expandable.toggleClass('active');
-					if (settings['animate']) {
-						expandable_content.animate(settings['animation_params'], settings['animation_speed']);
-					}
-					else {
-						expandable_content.toggle();
-					}
-					if (expandable_supporters_label) {
-						if (settings['animate']) {
-							expandable_supporters_label.animate(settings['animation_params'], settings['animation_speed']);
-						}
-						else {
-							expandable_supporters_label.toggle();
-						}
-					}
-				} else if (!expandable.hasClass('pending')) {
-				  expandable.addClass('pending');
-			  	// Load content now
-					if (expandable_loading.length > 0) {
-						expandable_loading.show();
-					}
-					else {
-						expandable_loading = $('<span/>').addClass('loading');
-						expandable_loading.insertAfter(expandable);
-					}
-					$.ajax({
-						url: path,
-						type: 'get',
-						dataType: 'script',
-						success: function(){
-							activate();
-						},
-						error: function(){
-							expandable_loading.hide();
-							expandable.removeClass('pending');
-						}
-					});
-				}
-			}
-
-      /*
-       * Designates the expandable as active.
-       */
-      function activate() {
-        expandable_loading.hide();
-        expandable_content = expandable_parent.children('.expandable_content');
-        if (expandable_content.length > 0) {
-          expandable.addClass('active');
-        }
-        if (expandable_supporters_label) {
-          if (settings['animate']) {
-            expandable_supporters_label.animate(settings['animation_params'], settings['animation_speed']);
-          }
-          else {
-            expandable_supporters_label.toggle();
-          }
-        }
-        expandable.removeClass('pending');
-      }
-
-      /**************/
-			/* Public API */
-      /**************/
-
-      $.extend(this,
-      {
-        reinitialise: function() {
-          initialise();
-        },
-				toggle: function() {
-					toggleExpandable();
-				},
+        	expandable.removeClass('pending');
+      	},
+      	toggle: function() {
+      		var that = this;
+			that._toggleExpandable();
+			return that;
+		},
         activated: function() {
-          activate();
+        	var that = this;
+          	that._activate();
+          	return that;
         },
-				isLoaded: function() {
-					return expandable_content != null && expandable_content.length > 0 && expandable_content.is(":visible");
-				}
-      });
-    }
-
-  };
-
-})(jQuery);
+		isLoaded: function() {
+			var that = this;
+			return that.expandableContent != null && that.expandableContent.length > 0 && that.expandableContent.is(":visible");
+		}
+		
+	});
+}( jQuery ) );
