b00d973966e8c37630e3c5a7df7b2289cfb1324e
Tiago Cardoso <tiago.cardoso@echologic.org>
2012-02-05 16:55:19 +0100

    rewrote the discuss search js plugin to a widget

--- public/javascripts/discuss/statement_search.js
+++ public/javascripts/discuss/statement_search.js
@@ -1,167 +1,126 @@
 /**
- * @author Tiago
+ * @author Tiagoo
  */
 
-(function($) {
+(function( $ ) {
+	$.widget("echo.statement_search", {
+		options: {
+			animation_speed: 300,
+			per_page : 7
+		},
+		_create: function() {
+			var that = this,
+				searchContainer = that.element;
+			that.elementsList = searchContainer.find('ul');
+			that.pagination = searchContainer.find('.more_pagination');
+			that.actionBar = $('#action_bar');
+			
+				that._initEchoIndicators(searchContainer);
+			that._initMoreButton();
+			that._initEmbedButton();
+			//that._initScrollPane();
+			
+		},
+		_initEchoIndicators: function(container) {
+        	container.find('.echo_indicator').each(function() {
+          		var indicator = $(this);
+          		if (indicator.hasClass("ein_initialized")) return true;
+          		
+            	indicator.progressbar({ value: parseInt(indicator.attr('alt')) });
+          		indicator.addClass("ei_initialized");
+        	});
+      	},
+      	// Handles the click on the more Button event (replaces it with an element of class 'more_loading')
+      	_initMoreButton: function() {
+      		var that = this,
+      			searchContainer = that.element,
+				elementsCount = searchContainer.find('li.question').length;
+				
+        	searchContainer.find(".more_pagination a:Event(!click)").addClass('ajax').bind("click", function(e) {
+        		e.preventDefault();
+        		e.stopPropagation();
+          		var moreButton = $(this),
+					loadingMoreButton = $('<span class="more_loading"/>').text(moreButton.text()),
+					pageCount = elementsCount / that.options.per_page + 1;
+          		moreButton.replaceWith(loadingMoreButton);
+				$.setFragment({"page_count" : pageCount, "page" : ""});
 
-  $.fn.statement_search = function(current_settings) {
+				// load elements that have to be updated on the page count parameter
+				var elementsToUpdate = searchContainer.find('a.statement_link, a.avatar_holder, a.add_new_button');
+				$.ajax({
+					url: moreButton.attr('href'),
+            		type: 'get',
+            		dataType: 'script',
+            		success: function(){
+						elementsToUpdate.each(function(){
+							that._updateUrlCount($(this), pageCount);
+						});
+			      	},
+					error: function(){
+						loadingButton.replaceWith(moreButton);
+			      	}
+				});
+        	});
+       	},
+       	_updateUrlCount: function(element, pageCount) {
+			element.attr('href',encodeURI(decodeURI(element.attr('href')).replace(/\|\d+/g, "|" + pageCount)));
+		},
+		// Initialises the Embed echo button and panel.
+      	_initEmbedButton: function() {
+      		var that = this,
+        		embedCode = that.actionBar.find('.embed_code');
+        	
+        	that.actionBar.find('#embed_link').bind("click", function(e) {
+        		e.preventDefault();
+          		$(this).next().animate({'opacity' : 'toggle'}, that.options.animation_speed);
+          		embedCode.selText();
+        	});
+        	that.actionBar.find('.embed_panel').bind("mouseleave", function() {
+          		$(this).fadeOut();
+        	});
+      	},
+      	insertContent: function(content, page) {
+      		var that = this,
+				childrenList = that.element.find(".content");
+			if (page == 1) 
+				childrenList.children().remove();
+				
+			childrenList.append(content);
 
-    $.fn.statement_search.defaults = {
-      'animation_speed': 300,
-			'per_page' : 7
-    };
-
-    // Merging settings with defaults
-    var settings = $.extend({}, $.fn.statement_search.defaults, current_settings);
-
-    return this.each(function() {
-      // Creating and binding the statement API
-      var elem = $(this), statementSearchApi = elem.data('searchApi');
-      if (statementSearchApi) {
-        statementSearchApi.reinitialise(current_settings);
-      } else {
-        statementSearchApi = new StatementSearch(elem);
-        elem.data('searchApi', statementSearchApi);
-      }
-    });
-
-
-    /*************************/
-    /* The statement handler */
-    /*************************/
-
-    function StatementSearch(search_container) {
-			var elements_list = search_container.find('ul');
-			var pagination = search_container.find('.more_pagination');
-			var action_bar = $('#action_bar');
-      // Initialize the statement
-      initialise();
-
-
-      // Initializes the statement.
-      function initialise() {
-        initEchoIndicators(search_container);
-				initMoreButton();
-				initEmbedButton();
-				//initScrollPane();
-      }
-
-			function initEchoIndicators(container) {
-        container.find('.echo_indicator').each(function() {
-          var indicator = $(this);
-          if (!indicator.hasClass("ei_initialized")) {
-            var echo_value = parseInt(indicator.attr('alt'));
-            indicator.progressbar({ value: echo_value });
-          }
-          indicator.addClass("ei_initialized");
-        });
-      }
-
-			/*
-       * Handles the click on the more Button event (replaces it with an element of class 'more_loading')
-       */
-      function initMoreButton() {
-				var elements_count = search_container.find('li.question').length;
-        search_container.find(".more_pagination a:Event(!click)").addClass('ajax').bind("click", function() {
-          var moreButton = $(this);
-					var loadingMoreButton = $('<span/>').text(moreButton.text()).addClass('more_loading');
-					var page_count = elements_count / settings['per_page'] + 1;
-          moreButton.replaceWith(loadingMoreButton);
-					$.setFragment({"page_count" : page_count, "page" : ""});
-
-					// load elements that have to be updated on the page count parameter
-					var elements_to_update = search_container.find('a.statement_link, a.avatar_holder, a.add_new_button');
-					$.ajax({
-						url: moreButton.attr('href'),
-            type: 'get',
-            dataType: 'script',
-            success: function(){
-							elements_to_update.each(function(){
-								updateUrlCount($(this), page_count);
-							});
-			      },
-						error: function(){
-							loadingButton.replaceWith(moreButton);
-			      }
-					});
-					return false;
-        });
-      }
-
-			function updateUrlCount(element, page_count) {
-				element.attr('href',encodeURI(decodeURI(element.attr('href')).replace(/\|\d+/g, "|" + page_count)));
-			}
-
-
-      /*
-       * Initializes the Embed echo button and panel.
-       */
-      function initEmbedButton() {
-        var embed_code = action_bar.find('.embed_code');
-        action_bar.find('#embed_link').bind("click", function() {
-          $(this).next().animate({'opacity' : 'toggle'}, settings['animation_speed']);
-          embed_code.selText();
-          return false;
-        });
-        action_bar.find('.embed_panel').bind("mouseleave", function() {
-          $(this).fadeOut();
-          return false;
-        });
-      }
-
-
-      // Public API of statement
-      $.extend(this,
-      {
-        reinitialise: function(resettings)
-        {
-          settings = $.extend({}, resettings);
-          initialise();
-        },
-				insertContent: function(content, page)
-				{
-					var children_list = $("#questions_container .content");
-					if (page == 1) {
-				  	children_list.children().remove();
-				  }
-					children_list.append(content);
-
-          // Scrolling to the first new list element
-          if (page > 1) {
-            var first_new_id = "#" + $(content).first().attr("id");
-            $.scrollTo(first_new_id, 700);
-          }
-					initEchoIndicators(children_list);
-				},
-				updateMoreButton: function(content, to_insert)
-				{
-          if (to_insert) {
-				  	if (pagination && pagination.length > 0) {
-				  		pagination.replaceWith(content);
-				  	}
-				  	else {
-				  		content.insertAfter(elements_list);
-				  	}
-						pagination = content;
-				  	initMoreButton();
-				  } else {
-					  if (pagination) {
-							pagination.remove();
-							pagination = null;
-						}
-					}
-				},
-				updateEmbedButton: function()
-				{
-					action_bar = $('.action_bar');
-					initEmbedButton();
-					return this;
+          	// Scrolling to the first new list element
+          	if (page > 1) {
+            	var firstNewId = "#" + $(content).first().attr("id");
+            	$.scrollTo(firstNewId, 700);
+          	}
+			that._initEchoIndicators(childrenList);
+		},
+		updateMoreButton: function(content, toInsert) {
+			var that = this;
+        	if (toInsert) {
+				if (that.pagination && that.pagination.length > 0) {
+					that.pagination.replaceWith(content);
 				}
-      });
-    }
-
-  };
-
-})(jQuery);
-
+				else {
+					content.insertAfter(elements_list);
+				}
+				pagination = content;
+				that.initMoreButton();
+			} else {
+				if (that.pagination) {
+					that.pagination.remove();
+					that.pagination = null;
+				}
+			}
+		},
+		updateEmbedButton: function() {
+			var that = this;
+			
+			that.actionBar = $('#action_bar');
+			that._initEmbedButton();
+			return that;
+		}
+      
+		
+	});
+}( jQuery ) );
 
