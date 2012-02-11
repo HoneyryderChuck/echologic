75ec890ff29031ce394b5783263b439a6667b753
Tiago Cardoso <tiago.cardoso@echologic.org>
2012-02-11 15:15:12 +0100

    indented a file

--- public/javascripts/connect/connect.js
+++ public/javascripts/connect/connect.js
@@ -1,22 +1,26 @@
-/* Do init stuff. */
-$(document).ready(function () {
-  if ($('#echo_connect_search').length > 0) {
-  	loadAvatarHolders();
+(function($){
+
+	/* Do init stuff. */
+	$(document).ready(function () {
+	  if ($('#echo_connect_search').length > 0) {
+	  	loadAvatarHolders();
 		initScrollToProfile();
-  }
-});
-
-
-function loadAvatarHolders(){
-	  $('#connect_results').delegate('.profile.active .avatar_holder', 'click', function(){
+	  }
+	});
+	
+	
+	function loadAvatarHolders(){
+		$('#connect_results').delegate('.profile.active .avatar_holder', 'click', function(e){
+			e.preventDefault();
 			$.scrollTo('body', 400, function() {$('#profile_details_container').animate(toggleParams, 500)});
-	    $('.profile').removeClass('active');
-	    return false;
+		    $('.profile').removeClass('active');
 		});
-}
-
-function initScrollToProfile() {
-	if ($('#profile_details_container').is(":visible")) {
-		$.scrollTo('#results_anchor', 400);
 	}
-}
+	
+	function initScrollToProfile() {
+		if ($('#profile_details_container').is(":visible")) {
+			$.scrollTo('#results_anchor', 400);
+		}
+	}
+
+})(jQuery);
