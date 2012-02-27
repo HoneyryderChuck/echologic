f8fc940d117877cb336082fc64e4938627992e2f
Laszlo Papp <laszlo.papp@echologic.org>
2012-02-27 01:44:23 +0100

    More JS adjustments.

--- public/javascripts/app/placeholder.js
+++ public/javascripts/app/placeholder.js
@@ -1,118 +1,133 @@
-(function( $ ) {
-	
-	$.widget("echo.placeholder", {
-		options: {
-			text_class    : "iframe.rte_doc",
-			text_area_class : ".statement_text",
-			changed_class : "changed",
-			default_attr  : "data-default",
-			focus_class   : "focused"
-		},
-		_create: function() {
-			var that = this,
-				place = that.element;
-				
-			that.textInputs = place.find("input[type='text']");
-			that.textAreas = isMobileDevice() ? place.find('textarea' + that.options.text_area_class) : place.find(that.options.text_class);
-				
-			that._loadInputs();
-			that._loadText();
-			that._loadSubmitClean();
-			
-		},
-		_loadInputs: function() {
-			var that = this;
-			that.textInputs.each(function(index){
-				var inputText = $(this),
-					initValue = inputText.val(),
-          			value = inputText.attr(that.options.default_attr);
-          			
-          		inputText.toggleVal({
-            		populateFrom: 'custom',
-            		text: value,
-					changedClass: that.options.changed_class,
-					focusClass: that.options.focus_class
-          		});
-          		inputText.removeAttr(that.options.default_attr);
-          		if (index == 0) inputText.blur();
-          		
-          		if (initValue && initValue.length > 0) {
-					inputText.val(initValue);
-					inputText.focus();
-					inputText.blur();
-				}
-			});
-		},
-		_loadText: function() {
-			var that = this;
-			if (isMobileDevice()) {
-          		// Simple text area (so that mobile devices detect it as input field)
-				that.textAreas.each(function(){
-					var textArea = $(this),
-						initValue = textArea.val(),
-					  	value = textArea.attr(that.options.default_attr);
-					  	
-            		value = value.replace(/(<([^>]+)>)/ig,""); // Stripping HTML tags
-					textArea.toggleVal({
-	            		populateFrom: 'custom',
-	            		text: value,
-	            		changedClass: that.options.changed_class,
-	            		focusClass: that.options.focus_class
-	          		});
-						
-					textArea.removeAttr(that.options.default_attr);
-					
-					if (initValue && initValue.length > 0) {
-	            		inputText.val(initValue);
-	            		inputText.focus();
-	            		inputText.blur();
-	          		}
-				});
-			} else {
-				// Text Area (RTE Editor)
-				that.textAreas.each(function(){
-					var editor = $(this),
-						value = editor.attr(that.options.default_attr),
-						doc = $(editor.contents().get(0)),
-						text = $(doc).find('body');
-						if (text && text.html().length == 0) {
-							var label = $("<span class='defaultText'/>").html(value);
-							label.insertAfter(editor).click(function(){
-								doc.click();
-							});
+(function($) {
 
-							doc.bind('click', function(){
-								label.hide();
-								editor.focus();
-							});
-							doc.bind('blur', function(){
-								var newText = $(editor.contents().get(0)).find('body');
-								if (newText.html().length == 0) 
-									label.show();
-							});
-						}
-						editor.removeAttr(that.options.default_attr);
-				});
-		    }
-		},
-		_loadSubmitClean: function() {
-			var that = this;
-			// Clean text inputs on submit
-        	that.element.bind('submit', (function() {
-					that._cleanToggleValues($(this));
-        	}));
-		},
-		_cleanToggleValues: function(content) {
-			content.find(".toggleval").each(function() {
-				var inp = $(this);
-          		if(inp.val() == inp.data("defText")) 
-            		inp.val("");
-        	});
-		},
-		cleanDefaultValues: function () {
-			var that = this;
-			that._cleanToggleValues(that.element);
-		}
-	});
-}( jQuery ) );
-		
+  $.widget("echo.placeholder", {
+    options: {
+      text_class    : "iframe.rte_doc",
+      text_area_class : ".statement_text",
+      changed_class : "changed",
+      default_attr  : "data-default",
+      focus_class   : "focused"
+    },
+
+    _create: function() {
+      var that = this,
+          place = that.element;
+
+      that.textInputs = place.find("input[type='text']");
+      that.textAreas = isMobileDevice() ?
+                         place.find('textarea' + that.options.text_area_class) : place.find(that.options.text_class);
+
+      that._loadInputs();
+      that._loadText();
+      that._loadSubmitClean();
+
+    },
+
+    _loadInputs: function() {
+      var that = this;
+      that.textInputs.each(function(index) {
+        var inputText = $(this),
+            initValue = inputText.val(),
+            value = inputText.attr(that.options.default_attr);
+
+        inputText.toggleVal({
+          populateFrom: 'custom',
+          text: value,
+          changedClass: that.options.changed_class,
+          focusClass: that.options.focus_class
+        });
+
+        inputText.removeAttr(that.options.default_attr);
+        if (index == 0) inputText.blur();
+
+        if (initValue && initValue.length > 0) {
+          inputText.val(initValue);
+          inputText.focus();
+          inputText.blur();
+        }
+      });
+    },
+
+    _loadText: function() {
+      var that = this;
+
+      if (isMobileDevice()) {
+        // Simple text area (so that mobile devices detect it as input field)
+        that.textAreas.each(function() {
+          var textArea = $(this),
+              initValue = textArea.val(),
+              value = textArea.attr(that.options.default_attr);
+
+          value = value.replace(/(<([^>]+)>)/ig, "");
+          // Stripping HTML tags
+          textArea.toggleVal({
+            populateFrom: 'custom',
+            text: value,
+            changedClass: that.options.changed_class,
+            focusClass: that.options.focus_class
+          });
+
+          textArea.removeAttr(that.options.default_attr);
+
+          if (initValue && initValue.length > 0) {
+            inputText.val(initValue);
+            inputText.focus();
+            inputText.blur();
+          }
+        });
+
+      } else {
+
+        // Text Area (RTE Editor)
+        that.textAreas.each(function() {
+          var editor = $(this),
+              value = editor.attr(that.options.default_attr),
+              doc = $(editor.contents().get(0)),
+              text = $(doc).find('body');
+          if (text && text.html().length == 0) {
+            var label = $("<span class='defaultText'/>").html(value);
+            label.insertAfter(editor).click(function() {
+              doc.click();
+            });
+
+            doc.bind('click', function() {
+              label.hide();
+              editor.focus();
+            });
+            doc.bind('blur', function() {
+              var newText = $(editor.contents().get(0)).find('body');
+              if (newText.html().length == 0)
+                label.show();
+            });
+          }
+          editor.removeAttr(that.options.default_attr);
+        });
+      }
+    },
+
+    _loadSubmitClean: function() {
+      var that = this;
+
+      // Clean text inputs on submit
+      that.element.bind('submit', (function() {
+        that._cleanToggleValues($(this));
+      }));
+    },
+
+    _cleanToggleValues: function(content) {
+      content.find(".toggleval").each(function() {
+        var inp = $(this);
+        if (inp.val() == inp.data("defText")) {
+          inp.val("");
+        }
+      });
+    },
+
+    cleanDefaultValues: function () {
+      var that = this;
+      that._cleanToggleValues(that.element);
+    }
+
+  });
+
+}(jQuery) );
--- public/javascripts/app/search.js
+++ public/javascripts/app/search.js
@@ -1,135 +1,141 @@
-(function( $ ) {
-	
-	$.widget("echo.main_search", {
-		options: {
-			
-		},
-		_create: function() {
-			var that = this,
-			searchForm = that.element;
+(function($) {
 
-			that.form = $("#search_form");
-			that.submitButton = that.form.find('.submit_button'); 
-			that.termsInput = $('#search_terms');
-			that.sortTypeInput = searchForm.find(':input[id=sort]');
-			
-			
-			that.searchPath = document.location.href.split('?')[0];
-			
-			that.form.placeholder();
-			
-			that._initSubmitButton();
-			that._initHashChange();	
-			that._loadSearchAutoComplete();
-			
-			that._refresh();
-			
-			$("#questions_container").statement_search();
-		},
-		_refresh: function() {
-			var that = this,
-			searchForm = that.element;
-			
-			that.sortTypeButtons = searchForm.parent().find("a.ajax_sort, a.ajax_no_sort");
-			that.paginationButtons = searchForm.parent().find('.pagination a');
-			
-			that._initHistoryEvents();
-			that._paginationButtons();
-		},
-		_initSubmitButton: function() {
-			var that = this;
-			
-			that.submitButton.bind("click", function(e){
-				e.preventDefault();
-				e.stopPropagation();
-			    that._setSearchHistory();
-			});
-		},
-		_initHistoryEvents: function() {
-			var that = this;
-			
-			that.termsInput.bind("keypress", function(e) {
-			    if (e && e.keyCode == $.ui.keyCode.ENTER) { /* check if enter was pressed */
-			    	that.submitButton.click();
-			      	return false;
-			    }
-		  	});
+  $.widget("echo.main_search", {
+    options: {
+    },
 
-  			that.sortTypeButtons.bind("click", function(e) {
-  				e.preventDefault();
-  				e.stopPropagation();
-  				var sortButton = $(this),
-    				sort = sortButton.hasClass('ajax_no_sort') ? '' : sortButton.attr('value');
+    _create: function() {
+      var that = this,
+          searchForm = that.element;
 
-				that.sortTypeInput.val(sort);
-				that.submitButton.click();
-  			});
+      that.form = $("#search_form");
+      that.submitButton = that.form.find('.submit_button');
+      that.termsInput = $('#search_terms');
+      that.sortTypeInput = searchForm.find(':input[id=sort]');
 
-		},
-		_paginationButtons: function() {
-			var that = this;
-			that.paginationButtons.bind("click", function(e) {
-				e.preventDefault();
-				e.stopPropagation();
-			    pushState({ "page" : $.deparam.querystring(this.href).page })
-			});
-		},
-		_initHashChange: function() {
-			var that = this;
-			if (window.searchHashHandling) return;
-			
-			$(window).bind("hashchange", function() {
-				// page_count is for the lazy more pagination
-	  			if (getState("page") && getState("page_count")) that._triggerSearchQuery();
-			});
-				
-			if (getState("page_count")) 
-			    pushState({"page": "1"});
-				
-			if (hashHistoryState()) {
-				echoApp.stateChangeStrategy = history.replaceState;
-				$(window).bind("popstate", function() {
-					echoApp.hash_state = $.deparam.querystring(location.href);
-					$(window).trigger( 'hashchange' );					
-			    });
-			}
-			
-			window.searchHashHandling = true;
-		},
-		_setSearchHistory: function() {
-			var that = this;
-			that.form.data('placeholder').cleanDefaultValues();
-			
-  			var searchTerms = $.trim(that.termsInput.val());
-  			
-  			var data = { "search_terms": searchTerms, 
-    					 "page": 1, 
-    					 "page_count" : ""
-    		};
-  			
-  			if (that.sortTypeInput.length > 0) 
-    			data["sort"] = that.sortTypeInput.val();
-    		
-	  		pushState(data);
-		},
-		// Initialises auto_complete property for the tags text input
-		_loadSearchAutoComplete: function() {
-			var that = this,
-				path = $('.function_container:first').is('#echo_discuss_search') ? 
-					   '../../discuss/auto_complete_for_tag_value' : 
-					   '../users/users/auto_complete_for_tag_value';
+      that.searchPath = document.location.href.split('?')[0];
+      that.form.placeholder();
 
-  			that.form.find('.tag_value_autocomplete').autocompletes(path, {minChars: 3,
-                                                                 		   selectFirst: false,
-                                                                 		   multiple: true});
-		},
-		_triggerSearchQuery: function() {
-			var that = this;
-			var state = getState();
-  			$.getScript($.param.querystring(that.searchPath, state), function() {
-  				that._refresh();
-  			});
-		}
-	});
-}( jQuery ) );
-	
+      that._initSubmitButton();
+      that._initHashChange();
+      that._loadSearchAutoComplete();
+
+      that._refresh();
+
+      $("#questions_container").statement_search();
+    },
+
+    _refresh: function() {
+      var that = this,
+          searchForm = that.element;
+
+      that.sortTypeButtons = searchForm.parent().find("a.ajax_sort, a.ajax_no_sort");
+      that.paginationButtons = searchForm.parent().find('.pagination a');
+
+      that._initHistoryEvents();
+      that._paginationButtons();
+    },
+
+    _initSubmitButton: function() {
+      var that = this;
+
+      that.submitButton.bind("click", function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        that._setSearchHistory();
+      });
+    },
+
+    _initHistoryEvents: function() {
+      var that = this;
+
+      that.termsInput.bind("keypress", function(e) {
+        if (e && e.keyCode == $.ui.keyCode.ENTER) { /* check if enter was pressed */
+          that.submitButton.click();
+          return false;
+        }
+      });
+
+      that.sortTypeButtons.bind("click", function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        var sortButton = $(this),
+            sort = sortButton.hasClass('ajax_no_sort') ? '' : sortButton.attr('value');
+
+        that.sortTypeInput.val(sort);
+        that.submitButton.click();
+      });
+    },
+
+    _paginationButtons: function() {
+      var that = this;
+      that.paginationButtons.bind("click", function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        pushState({ "page" : $.deparam.querystring(this.href).page })
+      });
+    },
+
+    _initHashChange: function() {
+      var that = this;
+      if (window.searchHashHandling) return;
+
+      $(window).bind("hashchange", function() {
+        // Page_count is for the lazy more pagination
+        if (getState("page") && getState("page_count")) that._triggerSearchQuery();
+      });
+
+      if (getState("page_count")) {
+        pushState({"page": "1"});
+      }
+
+      if (hashHistoryState()) {
+        echoApp.stateChangeStrategy = history.replaceState;
+        $(window).bind("popstate", function() {
+          echoApp.hash_state = $.deparam.querystring(location.href);
+          $(window).trigger('hashchange');
+        });
+      }
+      window.searchHashHandling = true;
+    },
+
+    _setSearchHistory: function() {
+      var that = this;
+      that.form.data('placeholder').cleanDefaultValues();
+
+      var searchTerms = $.trim(that.termsInput.val());
+
+      var data = { "search_terms": searchTerms,
+        "page": 1,
+        "page_count" : ""
+      };
+
+      if (that.sortTypeInput.length > 0)
+        data["sort"] = that.sortTypeInput.val();
+
+      pushState(data);
+    },
+
+    // Initialises auto_complete property for the tags text input
+    _loadSearchAutoComplete: function() {
+      var that = this,
+          path = $('.function_container:first').is('#echo_discuss_search') ?
+                   '../../discuss/auto_complete_for_tag_value' :
+                   '../users/users/auto_complete_for_tag_value';
+
+      that.form.find('.tag_value_autocomplete').autocompletes(path, {minChars: 3,
+        selectFirst: false,
+        multiple: true});
+    },
+
+    _triggerSearchQuery: function() {
+      var that = this;
+      var state = getState();
+      $.getScript($.param.querystring(that.searchPath, state), function() {
+        that._refresh();
+      });
+    }
+
+  });
+
+}(jQuery) );
